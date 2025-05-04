a3dacb919cad91fe4aa8d4b4dba04f07e75472a4
Andrea Zagarella <andrea.zagarella@hudl.com>
2025-05-04 17:43:09 +0200

    Bump versions Create multiple replicas to docker compose

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+const os = require('os');
 const express = require('express');
-const path = require('path')
-
 const app = express();
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('<h2>Hi there!</h2>');
 });
 
-app.use(express.static('public'));
 app.get('/html', (req, res) => {
   res.sendFile("sample_page.html", { root: __dirname });
-  console.log("sample html page rendered")
+  console.log("ciao! sample html page rendered")
+});
+
+app.get('/hostname', (req, res) => {
+  res.send(`<h2>Hi I'm ${os.hostname()}!</h2>`);
+  console.log(`uela! I'm ${os.hostname()}`)
 });
 
 app.listen(3000);
